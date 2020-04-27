cc11da9e940e5c7c13dff7774bdd18269ba2dca4
marvin lewis <marvinlewis01@gmail.com>
2020-04-27 13:22:09 -0700

    added routes in server.js

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const sellerRouter = require('./data/routers/sellerRouter');
+const buyerRouter = require('./data/routers/buyerRouter');
+const listingRouter = require('./data/routers/listingRouter');
+const auctionRouter = require('./data/routers/auctionRouter');
+const bidRouter = require('./data/routers/bidRouter');
+
+
 
 const server = express();
 
@@ -10,6 +17,12 @@ server.use(morgan('dev'));
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+
+server.use('/api/seller', sellerRouter);
+server.use('/api/buyer', buyerRouter);
+server.use('/api/auction', auctionRouter);
+server.use('/api/bid', bidRouter);
+server.use('/api/listing', listingRouter);
 
 server.get('/', (req, res) => {
     res.status(200).json({
