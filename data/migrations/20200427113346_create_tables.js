a9bb726b83c313bf5f1fccfeb4e47e989c38aa69
unknown <jacob.e.england@gmail.com>
2020-04-27 12:08:01 -0500

    Added initial migration

--- data/migrations/20200427113346_create_tables.js
+++ data/migrations/20200427113346_create_tables.js
@@ -13,10 +13,33 @@ exports.up = function(knex) {
         tbl.string('email').notNullable().unique();
         tbl.string('password').notNullable();
     })
+    .createTable('auctions', tbl => {
+        tbl.increments();
+        tbl.date('end_time').notNullable();
+        tbl.string('seller_id').references('id').inTable('sellers').notNullable();
+    })
+    .createTable('listings', tbl => {
+        tbl.increments();
+        tbl.string('name').notNullable().unique();
+        tbl.blob('image');
+        tbl.string('description').notNullable();
+        tbl.float('starting_price').notNullable();
+        tbl.integer('auction_id').references('id').inTable('auctions').notNullable();
+    })
+    .createTable('bids', tbl => {
+        tbl.increments();
+        tbl.date('time').notNullable();
+        tbl.float('amount').notNullable();
+        tbl.integer('listing_id').references('id').inTable('listings').notNullable();
+        tbl.string('bidder_id').references('id').inTable('buyers').notNullable();
+    })
 };
 
 exports.down = function(knex) {
   return knex.schema
     .dropTableIfExists('sellers')
     .dropTableIfExists('buyers')
+    .dropTableIfExists('auctions')
+    .dropTableIfExists('listings')
+    .dropTableIfExists('bids')
 };
