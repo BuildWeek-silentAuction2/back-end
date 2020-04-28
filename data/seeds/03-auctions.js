52d79a2fb9d6dd7a13345c1e8133955452617bac
unknown <jacob.e.england@gmail.com>
2020-04-28 13:14:15 -0500

    Updated seed data with new IDs but all of them this time

--- data/seeds/03-auctions.js
+++ data/seeds/03-auctions.js
@@ -5,8 +5,8 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('auctions').insert([
-        {id: 1, end_time: '2020-05-25 16:30:00', seller_id: 'cb79794e-39eb-4d3c-b301-6d1dffaf2981'},
-        {id: 2, end_time: '2020-06-13 18:00:00', seller_id: 'b7cafac8-3ab7-4ae2-ba2a-b32bf1183efe'}
+        {id: 1, end_time: '2020-05-25 16:30:00', seller_id: '2d0f7c4c-fd2a-45bf-8fca-035890866996'},
+        {id: 2, end_time: '2020-06-13 18:00:00', seller_id: 'dfe87610-8b86-4f71-835f-54b9ebf2b123'}
       ]);
     });
 };
