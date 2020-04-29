d52009878bd8fb0437cba33d5554776d25d6c5fe
unknown <jacob.e.england@gmail.com>
2020-04-29 12:09:39 -0500

    Added name column to auction table

--- data/seeds/03-auctions.js
+++ data/seeds/03-auctions.js
@@ -5,8 +5,8 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('auctions').insert([
-        {id: 1, end_time: '2020-05-25 16:30:00', seller_id: '2d0f7c4c-fd2a-45bf-8fca-035890866996'},
-        {id: 2, end_time: '2020-06-13 18:00:00', seller_id: 'dfe87610-8b86-4f71-835f-54b9ebf2b123'}
+        {id: 1, name: 'Nancy\'s Yard Sale', end_time: '2020-05-25 16:30:00', seller_id: '2d0f7c4c-fd2a-45bf-8fca-035890866996'},
+        {id: 2, name: 'Girl Scout Troop #2213 Fundraiser', end_time: '2020-06-13 18:00:00', seller_id: 'dfe87610-8b86-4f71-835f-54b9ebf2b123'}
       ]);
     });
 };
